7f5df3596c927db9ced7f06bc15940d6102bd3ac
Li Wei <wei.g.li@intel.com>
2022-03-01 15:45:43 +0800

    Add RV test
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- src/raft_server.c
+++ src/raft_server.c
@@ -604,7 +604,7 @@ int raft_recv_requestvote(raft_server_t* me_,
         node = raft_get_node(me_, vr->candidate_id);
 
     /* Reject request if we have a leader */
-    if (me->leader_id != -1 && me->leader_id != raft_node_get_id(node) &&
+    if (me->leader_id != -1 && me->leader_id != vr->candidate_id &&
         me->timeout_elapsed < me->election_timeout)
     {
         r->vote_granted = 0;
